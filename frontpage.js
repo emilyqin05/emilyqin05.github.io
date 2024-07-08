31cde5464263b8909288758e9b33c75bd1513fc7
emilyqin05 <76915761+emilyqin05@users.noreply.github.com>
2024-07-08 02:47:25 -0700

    Add files via upload

---
+++ frontpage.js
@@ -0,0 +1,41 @@
+const zoomElement = document.querySelector(".zoom");
+let zoom = 0.5; // Initial zoom level set to zoomed out
+const ZOOM_SPEED = 0.05;
+const MIN_ZOOM = 0.5;
+const MAX_ZOOM = 2;
+
+document.body.style.overflow = 'scroll';
+
+// Apply the initial zoom level
+zoomElement.style.transform = `scale(${zoom})`;
+zoomElement.style.transformOrigin = "center 20%";
+
+
+
+document.addEventListener("wheel", function(e) {  
+    if(e.deltaY > 0 && zoom < MAX_ZOOM){    
+        zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;  
+    } else if(e.deltaY < 0 && zoom > MIN_ZOOM){    
+        zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;  
+    }
+
+    zoomElement.style.transform = `scale(${zoom})`;
+});
+
+const pictureOverlay = document.querySelector(".pictureOverlay");
+const aboutMePopup = document.getElementById("aboutMePopup");
+const closePopupButton = document.getElementById("closePopup");
+
+pictureOverlay.addEventListener("click", function() {
+    aboutMePopup.style.display = "block";
+});
+
+closePopupButton.addEventListener("click", function() {
+    aboutMePopup.style.display = "none";
+});
+
+const computerOverlay = document.querySelector(".computerOverlay");
+
+computerOverlay.addEventListener("click", function() {
+    window.location.href = "projects.html";
+});
